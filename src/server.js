da5e6cdde7aa5f0bb1662ec012fa73a8c098e37f
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-03-28 12:33:20 +0900

    make Router

--- src/server.js
+++ src/server.js
@@ -1,12 +1,24 @@
 import express from "express";
-//0️⃣=== const express = require("express");
+//0️⃣ === const express = require("express");
 
 const PORT = 4000;
 
 const app = express();
 //1️⃣ A top level fuction exported by the express modules.
 
-const handleListening = () => console.log(`Server listening on port http://localhost:${PORT} 🔥`);
+const handleHome = (req, res) => {
+    return res.send("HOME");
+};
+const handleLogin = (req, res) => {
+    return res.send("LOGIN");
+};
+//4️⃣ req(request object) and res(respond object) are given to us by <express>.
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+//3️⃣ Router, <get> is http method, After create express application
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🔥`);
 
 app.listen(PORT, handleListening);
 //2️⃣ Create PORT and callback.
