b87fc76f279b04d837787273ea05381e10125cac
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-03-28 17:50:45 +0900

    URL Parameters

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,9 @@ import { edit, see, upload, deleteVideo } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/remove", deleteVideo);
+videoRouter.get("/:id(\\d+)", see);
+// Add URL Parameter, regular expression.
+videoRouter.get("/:id(\\d+)/edit", edit);
+videoRouter.get("/:id(\\d+)/remove", deleteVideo);
 
 export default videoRouter;
