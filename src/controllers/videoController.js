75c3ba5c4eb3cc9b14c1e376fa0907cdce94e7e9
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-03-31 01:24:09 +0900

    search

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,7 @@
 import Video from "../models/Video";
 
 export const home = async (req, res) => {
-  const videos = await Video.find({});
+  const videos = await Video.find({}).sort({ createdAt: "desc" });
   return res.render("home", { pageTitle: "HOME", videos });
 };
 // ❗️export each, you can export one thing by <export default>.
@@ -39,8 +39,6 @@ export const postEdit = async (req, res) => {
   return res.redirect(`/videos/${id}`);
 };
 
-export const search = (req, res) => res.send("Search");
-
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload video" });
 
@@ -66,4 +64,17 @@ export const deleteVideo = async (req, res) => {
   const { id } = req.params;
   await Video.findByIdAndDelete(id);
   return res.redirect("/");
+};
+
+export const search = async (req, res) => {
+  const { keyword } = req.query;
+  let videos = [];
+  if (keyword) {
+    videos = await Video.find({
+      title: {
+        $regex: new RegExp(keyword, "i"),
+      },
+    });
+  }
+  return res.render("search", { pageTitle: "search", videos });
 };
