df9e3de85839b8235eb1111c644c48765b159452
haeeema@naver.com <83750390+haeeema@users.noreply.github.com>
2022-04-03 13:10:20 +0900

    github login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import fetch from "node-fetch";
 import bcrypt from "bcrypt";
 
 //----------------------------------------------------------------------------------------------------- JOIN
@@ -52,7 +53,7 @@ export const getLogin = (req, res) => {
 
 export const postLogin = async (req, res) => {
   const { username, password } = req.body;
-  const usernameFindOne = await User.findOne({ username });
+  const usernameFindOne = await User.findOne({ username, socialOnly: false });
   if (!usernameFindOne) {
     return res.status(400).render("login", {
       pageTitle: "Login",
@@ -71,12 +72,90 @@ export const postLogin = async (req, res) => {
   // ⭐️Initialize session
   return res.redirect("/");
 };
-
+//----------------------------------------------------------------------------------------------------- Github
+//1️⃣ 1. Request a user's GitHub identity
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false,
+    scope: "read:user user:email",
+  };
+  // Configuration object
+  const params = new URLSearchParams(config).toString();
+  // utility
+  const finalUrl = `${baseUrl}?${params}`;
+  return res.redirect(finalUrl);
+};
+//2️⃣ 2. Users are redirected back to your site by GitHub
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code,
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        // You can also receive the response in different formats if you provide the format in the Accept header.
+      },
+    })
+  ).json();
+  // Send POST request to finalUrl
+  // <fetch> gives us data first, and then we have to extract json from data.
+  //3️⃣ 3. Use the access token to access the API
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      return res.redirect("/login");
+    }
+    let user = await User.findOne({ email: emailObj.email });
+    if (!user) {
+      user = await User.create({
+        email: emailObj.email,
+        avatarUrl: userData.avatar_url,
+        socialOnly: true,
+        username: userData.login,
+        password: "",
+        name: userData.name,
+        location: userData.location,
+      });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    return res.redirect("/login");
+  }
+};
+//----------------------------------------------------------------------------------------------------- LOGOUT
 export const logout = (req, res) => {
   req.session.destroy();
   return res.redirect("/");
 };
 
 export const edit = (req, res) => res.send("Edit User");
-export const remove = (req, res) => res.send("Remove User");
 export const see = (req, res) => res.send("See User");
--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,11 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
+  avatarUrl: String,
+  socialOnly: { type: Boolean, default: false },
+  // For check user logged-in by Github
   username: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String },
   name: { type: String, required: true },
   location: String,
 });
